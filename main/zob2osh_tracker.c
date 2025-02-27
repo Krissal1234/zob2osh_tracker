d0503f6a87e2c669b963dd6ab51952354c100acb
krissal1234 <salibakris03@gmail.com>
2025-02-27 23:11:38 +0100

    integrated wifi manager, that can connect to hotspot

--- main/zob2osh_tracker.c
+++ main/zob2osh_tracker.c
@@ -2,7 +2,8 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "sd_logger.h"
+#include "sd_card/sd_logger.h"
+#include "network/wifi_manager.h"
 #include "esp_log.h"
 
 void app_main() {
@@ -16,6 +17,15 @@ void app_main() {
 
     test_write_file();
     test_read_file();
+
+    esp_err_t status = connect_wifi();
+    if(status == WIFI_SUCCESS){
+      ESP_LOGI("Main", "ESP connected successfully");
+    }
+    else{
+      ESP_LOGI("Main","ESP connection failed");
+    }
+
     while(1){
       vTaskDelay(1000);
     }
